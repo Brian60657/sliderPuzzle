65cac5239ecbd2cee3214fc9e0f59e305ac8c488
Brian Schleifer <brianschleifer@gmail.com>
2017-02-07 12:32:53 -0800

    reverted display back and updated info from Kate

--- js/main.js
+++ js/main.js
@@ -16,3 +16,32 @@ function moveNumber(){
 // move right
 
 // update where the empty box is
+
+// check for win //////////////////////////
+// var boxes = $('.cell')
+// boxes.eq(0)
+//
+// var cellOrder = []
+// for (var i-0; i< boxes.length; i++){
+// 	cellOrder.push (boxes.eq(i).text())
+// }
+// if(cellOrder.join('') == "12345678")
+// return "WIN!"
+///////////////////////////////////////////
+
+// some row/column logic from Kate
+// var $boxes = $('.cell')
+// $('.cell').click(function(){
+//
+// 	var pos = $(this).attr('dataPosition')
+// 	console.log('row is ' + parseInt(pos[0]))
+// 	console.log('column is ' + parseInt(pos[2]))
+//
+// 	for (var i = 0; i< $boxes.length; i++){
+// 		if ($boxes.eq(i).attr('dataPosition')[0]== parseInt(pos[0])){
+// 			console.log('same row')
+// 		} else if ($boxes.eq(i).attr('dataPosition')[2]== parseInt(pos[2])){
+// 			console.log('same column')
+// 		}
+// 	}
+// })
