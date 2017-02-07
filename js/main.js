ff7268a53178914e2fda209a8b9ecef019ade9c2
Brian Schleifer <brianschleifer@gmail.com>
2017-02-07 15:56:37 -0800

    table for cells, not moving the numbers using inner.html to replace them

--- js/main.js
+++ js/main.js
@@ -1,47 +1,19 @@
-// used to let us know where the empty box is
-var emptyPositionRow = 1
-var emptyPositionColumn = 2
+var emptyRow = 3
+var emptyColumn = 3
 
-// get position of the cell being selected
-function moveNumber(){
+var puzzle = document.getElementById("puzzle")
 
+function replace(x) {
+	var row = x.parentElement.rowIndex
+  var column = x.cellIndex
+
+		if ((emptyColumn == column && Math.abs(emptyRow - row) == 1) || (emptyRow == row && Math.abs(emptyColumn - column) == 1)) {
+			var x2 = document.getElementById("puzzle").rows[emptyRow].cells[emptyColumn]
+
+			x2.innerHTML = x.innerHTML
+      x.innerHTML = " "
+
+			emptyRow = row
+      emptyColumn = column
+  	}
 }
-
-// move up
-
-// move down
-
-// move left
-
-// move right
-
-// update where the empty box is
-
-// check for win //////////////////////////
-// var boxes = $('.cell')
-// boxes.eq(0)
-//
-// var cellOrder = []
-// for (var i-0; i< boxes.length; i++){
-// 	cellOrder.push (boxes.eq(i).text())
-// }
-// if(cellOrder.join('') == "12345678")
-// return "WIN!"
-///////////////////////////////////////////
-
-// some row/column logic from Kate
-// var $boxes = $('.cell')
-// $('.cell').click(function(){
-//
-// 	var pos = $(this).attr('dataPosition')
-// 	console.log('row is ' + parseInt(pos[0]))
-// 	console.log('column is ' + parseInt(pos[2]))
-//
-// 	for (var i = 0; i< $boxes.length; i++){
-// 		if ($boxes.eq(i).attr('dataPosition')[0]== parseInt(pos[0])){
-// 			console.log('same row')
-// 		} else if ($boxes.eq(i).attr('dataPosition')[2]== parseInt(pos[2])){
-// 			console.log('same column')
-// 		}
-// 	}
-// })
