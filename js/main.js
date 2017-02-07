947e93daa0a1b8a517448d5d65db0b5c9c463dcf
Brian Schleifer <brianschleifer@gmail.com>
2017-02-06 16:52:09 -0800

    Added styling and number shuffle

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-<!-- Slider Puzzle -->
+// Slider Puzzle
+
+var array = (['1', '2' , '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15','-'])
+
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+  }
