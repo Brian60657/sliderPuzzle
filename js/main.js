782148b82ebe91c50364493040bc113ccedcb154
Brian Schleifer <brianschleifer@gmail.com>
2017-02-08 10:55:50 -0800

    Completed check for winner

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
+//gets numbers from HTML table and moves them to an empty cell(row/column)//////
 var emptyRow = 3
 var emptyColumn = 3
-
 var puzzle = document.getElementById("puzzle")
 
-// move numbers around in table
 function replace(x) {
 	var row = x.parentElement.rowIndex
   var column = x.cellIndex
@@ -17,30 +16,17 @@ function replace(x) {
 			emptyRow = row
       emptyColumn = column
   	}
-//check for win option, i think this is done
-		var boxes = $('#puzzle')
-		boxes.eq(0)
+
+//checks for win////////////////////////////////////////////////////////////////
+		var boxes = $('td')
+			boxes.eq(0)
 
 		var puzzleOrder = []
-		for (var i=0; i< boxes.length; i++){
-			puzzleOrder.push (boxes.eq(i).text())
-		}
-
-		if(puzzleOrder.join('') == "123456789101112131415")
+			for (var i=0; i< boxes.length; i++){
+				puzzleOrder.push(parseInt(boxes.eq(i).text()))
+}
+//console.log(puzzleOrder)
+		if(puzzleOrder.join('') == "123456789101112131415Nan")
 			alert("YOU WON!")
 }
-
-// check for winner, not complete
-// function determineWinner() {
-//
-// var check  = new Array(4)
-// 	check[0] = new Array (1, 2, 3, 4)
-// 	check[1] = new Array (5, 6, 7, 8)
-// 	check[2] = new Array (9, 10, 11, 12)
-// 	check[3] = new Array (13, 14, 15, 0)
-//
-//   	for (i = 0; i < 4; i++){
-//     	if (rows[i] == check[i]){
-// 			winner = false
-//     		else
-//         alert("Congratulations! You've Won!")
+//adds timer to puzzle//////////////////////////////////////////////////////////
